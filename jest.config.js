b98d3d8561e188e255973acf0198b90e7f3726e8
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-07-20 14:42:37 +0200

    chore: fix files to be includes in jest coverage

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
   testEnvironment: "jsdom",
   testRegex: "/(__tests__|tests)/.*\\.(test|spec)?\\.(js|jsx)$",
   moduleFileExtensions: ["js", "jsx", "json"],
-  collectCoverageFrom: ["lib/**/*.js"]
+  collectCoverageFrom: ["lib/*.js"]
 };
