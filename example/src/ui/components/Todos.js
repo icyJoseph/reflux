5502e23f4c1f45047486a323eadb66f562b4ccef
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-07-21 19:35:55 +0200

    refactor: remove unnecessary Draggable context

--- example/src/ui/components/Todos.js
+++ example/src/ui/components/Todos.js
@@ -1,11 +1,10 @@
 import React from "react";
 
-import { Draggable, Droppable } from "react-beautiful-dnd";
+import { Droppable } from "react-beautiful-dnd";
 
 import TodoList from "./TodoList";
 import { TODO_TYPES } from "../../data/constants";
 
-// renders a TodoList for each status in STATUSES
 export function Todos() {
   return (
     <Droppable droppableId="main">
@@ -21,22 +20,7 @@ export function Todos() {
             }}
           >
             {TODO_TYPES.map((status, index) => (
-              <Draggable key={status} draggableId={`${status}`} index={index}>
-                {(
-                  { innerRef, droppableProps, dragHandleProps, placeholder },
-                  snapshot
-                ) => (
-                  <>
-                    <TodoList
-                      ref={innerRef}
-                      droppableProps={droppableProps}
-                      dragHandleProps={dragHandleProps}
-                      status={status}
-                    />
-                    {placeholder}
-                  </>
-                )}
-              </Draggable>
+              <TodoList key={status} status={status} />
             ))}
           </div>
           {provided.placeholder}
