103c46baf7ade3ea54ec320bd243b9537578bccf
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-07-21 21:54:54 +0200

    feat: save to localStorage when store changes

--- example/src/data/constants.js
+++ example/src/data/constants.js
@@ -4,3 +4,6 @@ export const REVIEW = "review";
 export const DONE = "done";
 
 export const TODO_TYPES = [NEW, ONGOING, REVIEW, DONE];
+
+export const LOCAL_STORAGE_KEY = "@@TODOS";
+export const EMPTY_LOCAL_STORAGE = "{}";
--- example/src/ui/components/Todos.js
+++ example/src/ui/components/Todos.js
@@ -4,14 +4,25 @@ import { useConnect } from "reflux";
 import TodoList from "./TodoList";
 import reducer from "../../reducers/todos";
 import initialState from "../../data/mock";
-import { TODO_TYPES } from "../../data/constants";
+import {
+  TODO_TYPES,
+  LOCAL_STORAGE_KEY,
+  EMPTY_LOCAL_STORAGE
+} from "../../data/constants";
+
+function selector(state) {
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(state));
+  return Object.values(state);
+}
+
+function getInitialState(initialState) {
+  const stored = localStorage.getItem(LOCAL_STORAGE_KEY) || EMPTY_LOCAL_STORAGE;
+  if (stored === EMPTY_LOCAL_STORAGE) return initialState;
+  return JSON.parse(stored);
+}
 
 export function Todos() {
-  const allTodos = useConnect({
-    reducer,
-    initialState,
-    selector: state => Object.values(state)
-  });
+  const allTodos = useConnect(reducer, initialState, selector, getInitialState);
 
   return (
     <div
